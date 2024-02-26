c71519a0eae99c8d6309172c84f74621ab478163
StecBartlomiej <stecb@student.agh.edu.pl>
2024-02-26 21:23:05 +0100

    Changed telescope keymaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,9 +1,10 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [h]elp' })
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [k]eymap' })
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [f]ile' })
+vim.keymap.set('n', '<leader>ss', builtin.oldfiles, { desc = '[?] Search recent file' })
+vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+
 
